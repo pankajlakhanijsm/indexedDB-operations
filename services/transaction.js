21c5c9b601d5cfabda6de9dea36deafe6159c2ce
pankaj <pankajlakhani1993@gmail.com>
2024-10-24 13:13:06 +0530

    add get list code updated

--- services/transaction.js
+++ services/transaction.js
@@ -7,8 +7,24 @@ export const transactionFunction = ({ db, dbObject }) => {
     console.warn(error);
   };
   const store = transactionObj.objectStore("human");
+  // addData();
+  getData();
+};
+
+const addData = (store) => {
   const request = store.add({ id: 4, data: "test" });
   request.onsuccess = (event) => {
+    console.log("data inserted successfully");
+  };
+  request.onerror = (e) => {
+    console.warn("error on data insertion", e.target.error);
+  };
+};
+
+const getData = (store) => {
+  const request = store.getAll();
+  request.onsuccess = (event) => {
+    debugger;
     console.log("data inserted successfully");
   };
   request.onerror = (e) => {
